698d98d63bbc2e913e6b4a53b68e853f5a125cb6
inejc <nejc.ilenic@gmail.com>
2017-05-05 21:56:26 +0200

    implement hessenberg reduction

--- numerical/eigen.py
+++ numerical/eigen.py
@@ -3,7 +3,7 @@ from numpy.random import rand
 from scipy.linalg import lu, solve_triangular, norm
 
 
-def inverse_iteration(A, eigval_approx, acc=1e-5):
+def inverse_iteration(A, eigval_approx, reduce=True, acc=1e-6):
     """Finds an approximate eigenvector for an approximation to a corresponding
     eigenvalue.
 
@@ -16,7 +16,12 @@ def inverse_iteration(A, eigval_approx, acc=1e-5):
     eigval_approx: float
         Initial approximation for the eigenvalue. Should be closer to the
         eigenvalue in interest than to any other eigenvalue.
-    
+
+    reduce: boolean
+        If true A is reduced to a similar Hessenberg matrix (or tridiagonal
+        matrix if A is symmetric) with Householder reflections prior to
+        calculating the eigenvector and the eigenvalue.
+
     acc: float
         Desired accuracy of the solution (i.e. the stopping condition).
 
@@ -28,6 +33,9 @@ def inverse_iteration(A, eigval_approx, acc=1e-5):
     eigenvalue: float
         Approximate eigenvalue.
     """
+    if reduce:
+        A, vectors_u = householder_reduce(A)
+
     I = np.identity(A.shape[0])
     A_shifted = A - eigval_approx * I
 
@@ -36,7 +44,7 @@ def inverse_iteration(A, eigval_approx, acc=1e-5):
     P, L, U = lu(A_shifted)
 
     while True:
-        # avoid computing the inverse of a shifted matrix
+        # avoid computing the inverse of a matrix (use LU decomposition instead)
         x = solve_triangular(L, P.dot(eigvec), lower=True)
         x = solve_triangular(U, x)
 
@@ -46,4 +54,49 @@ def inverse_iteration(A, eigval_approx, acc=1e-5):
         if norm((A - eigval * I).dot(eigvec), np.inf) < acc * norm(A, np.inf):
             break
 
+    if reduce:
+        eigvec = _dot_Q_x(vectors_u, eigvec)
+
     return eigvec, eigval
+
+
+def householder_reduce(A):
+    """Computes the reduction of a matrix to a similar Hessenberg matrix using
+    householder reflections. If input is symmetric reduction results in a
+    tridiagonal matrix.
+
+    Parameters
+    ----------
+    A: array-like, shape (n, n)
+        Matrix to reduce.
+
+    Returns
+    -------
+    H: array-like, shape (n, n)
+        Upper Hessenberg or tridiagonal matrix similar to A.
+
+    vectors_u: list, length n - 2
+        Vectors u that determine the Qi matrices.
+    """
+    H = A.copy()
+    vectors_u = []
+
+    for i in range(H.shape[0] - 2):
+        u = H[i + 1:, i].copy()
+        u[0] += np.sign(u[0]) * norm(u)
+        u = (u / norm(u)).reshape((-1, 1))
+        vectors_u.append(u)
+
+        # H = Q_i.T.dot(H)
+        H[i + 1:, i:] = H[i + 1:, i:] - 2 * u.dot(u.T.dot(H[i + 1:, i:]))
+        # H = H.dot(Q_i)
+        H[:, i + 1:] = H[:, i + 1:] - 2 * H[:, i + 1:].dot(u.dot(u.T))
+
+    return H, vectors_u
+
+
+def _dot_Q_x(vectors_u, x):
+    reversed_ = zip(range(len(vectors_u), 0, -1), reversed(vectors_u))
+    for i, u in reversed_:
+        x[i:] = x[i:] - 2 * u.dot(u.T.dot(x[i:]))
+    return x
--- tests/test_eigen.py
+++ tests/test_eigen.py
@@ -1,34 +1,77 @@
 from unittest import TestCase
 
 import numpy as np
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_array_almost_equal
+from scipy.linalg import norm
 
-from numerical.eigen import inverse_iteration
+from numerical.eigen import inverse_iteration, householder_reduce
 
 
 class PowerIterationTest(TestCase):
 
-    def setUp(self):
-        self.A = np.array([
+    def test_inverse_iteration(self):
+        A = np.array([
             [3, 3, 0],
             [3, 2, -7],
-            [0, -8, 6]])
+            [0, -8, 6]]).astype(float)
 
-        self.eigen_value_1_expected = -4.52
+        eigval_1_expected = -4.52
+        eigvec_1_expected = np.array([-0.30266, 0.75866, 0.57692])
+        eigvec, eigval = inverse_iteration(A, -5)
 
-        self.eigen_vector_1_expected = np.array([-0.30266, 0.75866, 0.57692])
-
-    def test_inverse_iteration(self):
-        eigen_vector, eigen_value = inverse_iteration(self.A, -5)
-
-        assert_almost_equal(
-            eigen_vector,
-            self.eigen_vector_1_expected,
-            decimal=4
-        )
+        self._assert_collinear(eigvec, eigvec_1_expected)
 
         self.assertAlmostEqual(
-            eigen_value,
-            self.eigen_value_1_expected,
+            eigval,
+            eigval_1_expected,
             places=2
         )
+
+    def test_hessenberg(self):
+        A = np.array([
+            [5, 1, 2, 0, 4],
+            [1, 4, 2, 1, 3],
+            [2, 2, 5, 4, 0],
+            [0, 1, 4, 1, 3],
+            [4, 3, 0, 3, 4]]).astype(float)
+
+        H_expected = np.array([
+            [5, -4.58258, 0, 0, 0],
+            [-4.58258, 5.71429, -5.60733, 0, 0],
+            [0, -5.60733, 1.8636, -2.36985, 0],
+            [0, 0, -2.36985, 3.7425, -1.51171],
+            [0, 0, 0, -1.51171, 2.67961]])
+
+        H, _ = householder_reduce(A)
+        assert_array_almost_equal(H, H_expected, decimal=5)
+
+        A = np.array([
+            [1, -4, 0, 3],
+            [-1, 3, 0, -2],
+            [3, -7, -2, 6],
+            [0, 4, 0, -2]]).astype(float)
+
+        H_expected = np.array([
+            [1, 1.26491, -0.507093, 4.8107],
+            [3.16228, 0.6, -1.17595, 9.97282],
+            [0, 1.49666, 0.4, 3.79473],
+            [0, 0, 0, -2]])
+
+        H, _ = householder_reduce(A)
+        assert_array_almost_equal(H, H_expected, decimal=5)
+
+    def test_reduction_no_reduction_same(self):
+        A = np.array([
+            [2, -1, 1],
+            [-1, 2, 1],
+            [1, -1, 2]]).astype(float)
+
+        eigvec_0, eigval_0 = inverse_iteration(A, 2.1, reduce=True)
+        eigvec_1, eigval_1 = inverse_iteration(A, 2.1, reduce=False)
+
+        self._assert_collinear(eigvec_0, eigvec_1)
+        self.assertAlmostEqual(eigval_0, eigval_1, places=4)
+
+    def _assert_collinear(self, x, y):
+        abs_cos = np.abs(x.dot(y) / (norm(x) * norm(y)))
+        self.assertAlmostEqual(abs_cos, 1)
