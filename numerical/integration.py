f2d13557fbd3e73e3b0f4eca3f1c422b23e828c2
inejc <nejc.ilenic@gmail.com>
2017-05-23 20:29:07 +0200

    refactor, implement gauss_quad_2, optimize standard normal cdf

--- numerical/integration.py
+++ numerical/integration.py
@@ -3,13 +3,6 @@ from os.path import join, dirname
 
 import numpy as np
 
-_DATA_DIR = join(dirname(dirname(__file__)), 'data')
-with open(join(_DATA_DIR, 'legendre.csv'), 'r') as f:
-    def line_to_tuple(l):
-        split = l.split(',')
-        return float(split[0]), float(split[1])
-    _LEGENDRE = [line_to_tuple(l) for l in list(f)]
-
 
 def std_norm_cdf(x):
     """Computes a cumulative distribution function of the standard normal
@@ -29,15 +22,15 @@ def std_norm_cdf(x):
 
     if x <= -3:
         u = 1 / x
-        return const * _gauss_quad(_propto_pdf_changed_var, 0, u, n=2)
+        return const * gauss_quad(_propto_pdf_changed_var, 0, u, num_points=15)
     elif -3 < x <= 0:
-        return .5 - const * _gauss_quad(_propto_pdf, x, 0, n=1)
+        return .5 - const * gauss_quad(_propto_pdf, x, 0, num_points=9)
     elif 0 < x <= 3:
-        return .5 + const * _gauss_quad(_propto_pdf, 0, x, n=1)
+        return .5 + const * gauss_quad(_propto_pdf, 0, x, num_points=9)
     else:
         # can't compute 1 - F because of catastrophic cancellation
-        F_0_3 = _gauss_quad(_propto_pdf, 0, 3, n=1)
-        F_3_x = _gauss_quad(_propto_pdf_changed_var, 1 / 3, 1 / x, n=2)
+        F_0_3 = gauss_quad(_propto_pdf, 0, 3, num_points=9)
+        F_3_x = gauss_quad(_propto_pdf_changed_var, 1 / 3, 1 / x, num_points=15)
         return .5 + const * (F_0_3 + F_3_x)
 
 
@@ -50,9 +43,9 @@ def _propto_pdf_changed_var(u):
     return - np.exp(- .5 * u ** -2) / u ** 2
 
 
-def _gauss_quad(func, a, b, n=2):
+def gauss_quad(func, a, b, num_points, n=1):
     """Approximates a definite integral of func on interval [a, b] using
-    Legendre-Gauss quadrature integral approximation (of degree 10).
+    Legendre-Gauss quadrature integral approximation.
 
     Parameters
     ----------
@@ -65,6 +58,10 @@ def _gauss_quad(func, a, b, n=2):
     b: float
         Upper bound of the integration.
 
+    num_points: int, one of (9, 15)
+        Number of weights and abscissae to use (i.e. Legendre roots
+        and coefficients).
+
     n: int
         Number of regions in which [a, b] is split.
 
@@ -73,11 +70,42 @@ def _gauss_quad(func, a, b, n=2):
     I_hat: float
         Estimate of the area under the curve.
     """
+    assert num_points in _LEGENDRE.keys()
+    legendre = _LEGENDRE[num_points]
+
     h_half = 0.5 * (b - a) / n
 
     I_hat = 0
     for i in range(n):
-        for w_j, x_j in _LEGENDRE:
+        for w_j, x_j in legendre:
             I_hat += w_j * func(h_half * (x_j + 2 * i + 1) + a)
 
     return h_half * I_hat
+
+
+def gauss_quad_2(func, a, b, n=1):
+    """Approximates a definite integral of func on interval [a, b] using
+    a vectorized Legendre-Gauss quadrature integral approximation of degree 2.
+    For parameters and return values see gauss_quad_9 above, but note that
+    func has to be a vectorized function.
+    """
+    h_half = 0.5 * (b - a) / n
+    x = sqrt(1 / 3)
+    i = 2 * np.arange(n) + 1
+    regions = func(h_half * (x + i) + a) + func(h_half * (-x + i) + a)
+
+    return h_half * np.sum(regions)
+
+
+def _line_to_tuple(l):
+    split = l.split(',')
+    return float(split[0]), float(split[1])
+
+_DATA_DIR = join(dirname(dirname(__file__)), 'data')
+_LEGENDRE = {}
+
+with open(join(_DATA_DIR, 'legendre_9.csv'), 'r') as f:
+    _LEGENDRE[9] = [_line_to_tuple(l) for l in list(f)]
+
+with open(join(_DATA_DIR, 'legendre_15.csv'), 'r') as f:
+    _LEGENDRE[15] = [_line_to_tuple(l) for l in list(f)]
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,36 +3,43 @@ from unittest import TestCase
 
 from scipy.stats import norm
 
-from numerical.integration import std_norm_cdf
+from numerical.integration import std_norm_cdf, gauss_quad_2
 
 
-class StandardNormalCdfTest(TestCase):
+class IntegrationTest(TestCase):
 
-    def test_negative_input(self):
+    def test_cdf_negative_input(self):
         self.assertAlmostEqual(norm.cdf(-1), std_norm_cdf(-1), places=10)
 
-    def test_zero_input(self):
+    def test_cdf_zero_input(self):
         self.assertAlmostEqual(norm.cdf(0), std_norm_cdf(0), places=10)
 
-    def test_positive_input(self):
+    def test_cdf_positive_input(self):
         self.assertAlmostEqual(norm.cdf(1), std_norm_cdf(1), places=10)
 
-    def test_one(self):
-        self.assertAlmostEqual(norm.cdf(1e6), std_norm_cdf(1e6), places=10)
+    def test_cdf_one(self):
+        self.assertAlmostEqual(norm.cdf(1e8), std_norm_cdf(1e8), places=10)
 
-    def test_zero(self):
-        self.assertAlmostEqual(norm.cdf(-1e6), std_norm_cdf(-1e6), places=10)
+    def test_cdf_zero(self):
+        self.assertAlmostEqual(norm.cdf(-1e8), std_norm_cdf(-1e8), places=10)
 
-    def test_half(self):
+    def test_cdf_half(self):
         self.assertAlmostEqual(0.5, std_norm_cdf(0), places=10)
 
-    def test_minus_three(self):
+    def test_cdf_minus_three(self):
         self.assertAlmostEqual(norm.cdf(-3), std_norm_cdf(-3), places=10)
 
-    def test_three(self):
+    def test_cdf_three(self):
         self.assertAlmostEqual(norm.cdf(3), std_norm_cdf(3), places=10)
 
-    def test_rand(self):
+    def test_cdf_rand(self):
         for _ in range(1000):
-            x = uniform(-1e6, 1e6)
+            x = uniform(-1e8, 1e8)
             self.assertAlmostEqual(norm.cdf(x), std_norm_cdf(x), places=10)
+
+    def test_gauss_quad_2(self):
+        self.assertEqual(gauss_quad_2(lambda x: x ** 2, -1, 1), 2 / 3)
+        self.assertEqual(gauss_quad_2(lambda x: x ** 3, -1, 1), 0)
+
+        def f(x): return x ** 3 - x ** 2 + x - 1
+        self.assertEqual(gauss_quad_2(f, -1, 1), - 8 / 3)
